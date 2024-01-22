f7d0de9af37427e54da9f70a998e10911d2156af
Ante Kolobaric <ak003522253@fer.hr>
2024-01-22 05:37:37 +0100

    Spojen pzo sustav drugog tima

---
+++ Rafale_v_PZO/Assets/Scripts/Kolizija.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Threading;
+
+public class Kolizija : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public AudioClip explosionSound;
+    public float explosionVolume = 1.5f;
+    
+    public RocketMovement rocketMovementScript;
+
+    void Start() {
+        rocketMovementScript = GetComponent<RocketMovement>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {   
+
+        rocketMovementScript.collision = true;
+
+        if (other.gameObject.tag == "Avion")
+        {
+            // Deactivate the current GameObject
+            gameObject.SetActive(false);
+            other.gameObject.SetActive(false);
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+            // Play the explosion sound
+            PlayExplosionSound();
+
+        }
+
+        if (other.gameObject.tag == "terrain")
+        {   
+            
+            // Deactivate the current GameObject
+            rocketMovementScript.MissileCancelInvoke();
+            gameObject.SetActive(false);
+            rocketMovementScript.isMoving = false;
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+            // Play the explosion sound
+            PlayExplosionSound();
+            Invoke("SetNewMissile", 1.5f);
+
+        }
+
+        
+    }
+
+    public void SetNewMissile() {
+        rocketMovementScript.SetNewMissile();
+        rocketMovementScript.collision = false;
+    }
+
+    private void PlayExplosionSound()
+    {
+         if (explosionSound != null)
+        {
+            // Create an empty GameObject to act as an audio source
+            GameObject audioSourceObject = new GameObject("ExplosionAudioSource");
+            AudioSource audioSource = audioSourceObject.AddComponent<AudioSource>();
+
+            // Set the audio clip and volume
+            audioSource.clip = explosionSound;
+            audioSource.volume = explosionVolume;
+
+            // Play the sound
+            audioSource.Play();
+
+            // Destroy the audio source object after the sound finishes
+            Destroy(audioSourceObject, explosionSound.length);
+        }
+    }
+}
